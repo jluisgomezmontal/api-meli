2312573a2b5b030754c9d97ec858f0413b1f761c
Jose Luis <montalgex18@gmail.com>
2023-08-29 15:53:19 -0600

    axios added

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,10 +1,11 @@
+const axios = require("axios");
+
 exports.mostrarProductos = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  console.debug(req.params.query);
   try {
     const url = `https://api.mercadolibre.com/sites/MLA/search?q=${req.params.query}`;
-    const response = await fetch(url);
-    const results = await response.json();
+    const response = await axios.get(url);
+    const results = response.data;
     const productos = results.results.map((result, idx) => {
       return {
         author: {
@@ -32,38 +33,47 @@ exports.mostrarProductos = async (req, res) => {
     console.log(error);
   }
 };
+
 exports.obtenerProducto = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const url = `https://api.mercadolibre.com/items/${req.params.id}`;
   const urlDescripcion = `https://api.mercadolibre.com/items/${req.params.id}/description`;
-  const response = await fetch(url);
-  const responseDescripcion = await fetch(urlDescripcion);
-  const result = await response.json();
-  const resultDescripcion = await responseDescripcion.json();
-  const urlCategoria = `https://api.mercadolibre.com/categories/${result.category_id}`;
-  const responseCategoria = await fetch(urlCategoria);
-  const resultCategotia = await responseCategoria.json();
-  const producto = {
-    author: {
-      name: "Jose Luis",
-      lastname: "Gomez",
-    },
-    item: {
-      id: result.id,
-      title: result.title,
-      price: {
-        currency: result.currency_id,
-        amount: parseInt(result.price, 10),
-        decimals: result.price.toString().split(".")[1],
-      },
-      picture: result.pictures[0].url,
-      condition: result.condition,
-      free_shipping: result.shipping.free_shipping,
-      sold_quantity: result.sold_quantity,
-      categoria: resultCategotia.name,
-      description: resultDescripcion.plain_text,
-    },
-  };
 
-  res.json(producto);
+  try {
+    const response = await axios.get(url);
+    const responseDescripcion = await axios.get(urlDescripcion);
+
+    const result = response.data;
+    const resultDescripcion = responseDescripcion.data;
+
+    const urlCategoria = `https://api.mercadolibre.com/categories/${result.category_id}`;
+    const responseCategoria = await axios.get(urlCategoria);
+    const resultCategoria = responseCategoria.data;
+
+    const producto = {
+      author: {
+        name: "Jose Luis",
+        lastname: "Gomez",
+      },
+      item: {
+        id: result.id,
+        title: result.title,
+        price: {
+          currency: result.currency_id,
+          amount: parseInt(result.price, 10),
+          decimals: result.price.toString().split(".")[1],
+        },
+        picture: result.pictures[0].url,
+        condition: result.condition,
+        free_shipping: result.shipping.free_shipping,
+        sold_quantity: result.sold_quantity,
+        categoria: resultCategoria.name,
+        description: resultDescripcion.plain_text,
+      },
+    };
+
+    res.json(producto);
+  } catch (error) {
+    console.log(error);
+  }
 };
