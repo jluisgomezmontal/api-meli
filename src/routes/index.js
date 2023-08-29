a6d1729d57a8ee1a6400eb65bb7b5b218d96aa61
Jose Luis <montalgex18@gmail.com>
2023-08-29 14:25:36 -0600

    eeeeeee

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const productosController = require("../controllers/productosController");
+
 module.exports = function () {
   // obtener productos
-  router.get("api/:query", productosController.mostrarProductos);
+  router.get("api/products", productosController.mostrarProductos);
+
   router.get("api/items/:id", productosController.obtenerProducto);
 
   router.get("/", (req, res) => {
