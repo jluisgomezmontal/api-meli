b3bf8601e0acf9e31e7fb8433920c89938204135
Jose Luis <montalgex18@gmail.com>
2023-08-29 14:18:44 -0600

    slash fixed

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const routes = require('./src/routes');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const routes = require("./src/routes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // crear server
 
 const app = express();
 
 // app routes
-app.use('/', routes());
+app.use("/", routes());
 app.use(cors());
 
 // port
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT)
-
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const productosController = require("../controllers/productosController");
 module.exports = function () {
   // obtener productos
-  router.get("/api/:query", productosController.mostrarProductos);
-  router.get("/api/items/:id", productosController.obtenerProducto);
+  router.get("api/:query", productosController.mostrarProductos);
+  router.get("api/items/:id", productosController.obtenerProducto);
 
   router.get("/", (req, res) => {
     res.send("mercado libre TEST");
